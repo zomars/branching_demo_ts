302294d1997ac826fa14e77ae386a21a2bae5741
zomars <zomars@me.com>
2022-07-14 18:32:13 -0600

    Update vercel.sh

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -18,17 +18,17 @@ if [ "$VERCEL_ORG_ID" == "" ]; then
   exit 0
 fi
 
-if [ "$PG_CREDENTIALS" == "" ]; then
-  echo "Error: PG_CREDENTIALS is empty"
+if [ "$NEON_PG_CREDENTIALS" == "" ]; then
+  echo "Error: NEON_PG_CREDENTIALS is empty"
   exit 0
 fi
 
-if [ "$PG_CLUSTER" == "" ]; then
-  echo "Error: PG_CLUSTER is empty"
+if [ "$NEON_PG_CLUSTER" == "" ]; then
+  echo "Error: NEON_PG_CLUSTER is empty"
   exit 0
 fi
 
-if [ "$API_TOKEN" == "" ]; then
+if [ "$NEON_API_TOKEN" == "" ]; then
   echo "Error: API_TOKEN is empty"
   exit 0
 fi
@@ -39,10 +39,10 @@ if [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then
 fi
 
 # create branch
-BRANCH_NAME=$(curl -o - -X POST -H "Authorization: Bearer $API_TOKEN" https://console.neon.tech/api/v1/projects/$PG_CLUSTER/branches | jq -r '.id')
+BRANCH_NAME=$(curl -o - -X POST -H "Authorization: Bearer $NEON_API_TOKEN" https://console.neon.tech/api/v1/projects/$NEON_PG_CLUSTER/branches | jq -r '.id')
 
 # switch to branch
-BRANCH_URL=$(postgres://$PG_CREDENTIALS@$BRANCH_NAME.cloud.neon.tech/main)
+BRANCH_URL=$(postgres://$NEON_PG_CREDENTIALS@$BRANCH_NAME.cloud.neon.tech/main)
 
 # We update DATABASE_URL using Vercel API
 curl -o - -X POST "https://api.vercel.com/v1/projects/$VERCEL_PROJECT_ID/env?teamId=$VERCEL_ORG_ID" \
